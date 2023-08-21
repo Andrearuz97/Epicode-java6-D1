6da393fa2a95bff760108dcf17fde2c83c33082b
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-21 17:03:56 +0200

    commit

---
+++ src/main/java/andrearuzittu/java6D1/Java6D1Application.java
@@ -0,0 +1,13 @@
+package andrearuzittu.java6D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java6D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/andrearuzittu/java6D1/Java6D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package andrearuzittu.java6D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
